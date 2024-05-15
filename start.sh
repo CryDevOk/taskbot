862d0474c2a041734fc440fbb1017be5080ad92f
test <johndoe@example.com>
2024-05-15 14:04:47 +0700

    conf

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/$USER/taskmanageraibot/
+cd /home/$USER/taskbot/
 docker-compose rm taskbot
 git pull
-docker build -t ghcr.io/igamingsolutions/tgbot:latest -f ./Dockerfile .
+docker build -t ghcr.io/igamingsolutions/taskbot:latest -f ./Dockerfile .
 docker-compose up taskbot
